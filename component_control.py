7c0f96f91561680149b69e4b50e4494db9b29212
Artem's guest PC <frederikbarnes@icloud.com>
2020-11-03 14:55:51 -0100

    outp? and switch test

--- component_control.py
+++ component_control.py
@@ -65,7 +65,7 @@ switch_address = 'GPIB0::'+switch_primary_address+'::INSTR'
 lockin_address = 'TCPIP::'+lockin_ip+'::'+lockin_lan_devicename+'::'+'INSTR'
 
 
-#create resource manager using py-visa backend ('@py') leave empty for NI VISA
+#create resource manager using py-visa backend ('@py') leave empty for NI VIS
 rm = pyvisa.ResourceManager()
 #print available devices (resources)
 print(rm.list_resources())
@@ -87,6 +87,8 @@ lockin.read_termination = '\n'
 switch.write('C') #C = Clear = open all relays or turn OFF all relays
 switch_status = switch.query('S') #S = Status which can be used on individual switch points, modules, or the entire system. Replies with string eg 0000000 showing 8 closed switches
 print(switch_status)
+switch.write('L3 5')
+print(switch.query('S'))
 
 #reset lock-in
 #check timebase is internal
@@ -101,8 +103,8 @@ print("Reference phase:", phase)
 
 #set frequency
 FREQ_INIT = 1e4
-lockin.write('FREQ '+str(FREQ_INIT)) #Set the internal frequency to FREQ_INIT
-freq = lockin.query('PHAS?') #Returns the internal frequency in Hz
+lockin.write('FREQINT '+ str(FREQ_INIT)) #Set the internal frequency to FREQ_INIT
+freq = lockin.query('FREQINT?') #Returns the internal frequency in Hz
 print("Frequency: ", freq)
 
 #set sine out voltage
@@ -111,6 +113,7 @@ lockin.write('SLVL '+str(VOUT_INIT)) #Set the sine out amplitude to FREQ_INIT in
 vout = lockin.query('SLVL?') #Returns the sine out amplitude in Volts
 print("Sine out amplitude: ", vout)
 
+'''
 #set channels
 lockin.write("COUT OCH1, XY") #Set CH1 to X
 lockin.write("COUT OCH2, XY") #Set CH2 to Y
@@ -120,6 +123,41 @@ y = lockin.query("COUT? OCH2") #Returns the CH2 output Y
 
 print('x:', x)
 print('y:', y)
+
+
+
+lockin.write("COUT OCH1, RTheta") #Set CH1 to X
+lockin.write("COUT OCH2, RTheta") #Set CH2 to Y
+
+r = lockin.query("COUT? OCH1") #Returns the CH1 output X
+theta = lockin.query("COUT? OCH2") #Returns the CH2 output Y
+
+print('r:', x)
+print('theta:', y)
+'''
+
+lockin.write("ARNG") #auto range 
+lockin.write("ASCL") #auto scale
+
+#CDSP
+
+#get X
+x = lockin.query('OUTP? X')
+print("X:",x)
+
+#get Y
+y = lockin.query('OUTP? Y')
+print("Y:",y)
+
+#get R
+r = lockin.query('OUTP? R')
+print("R:",r)
+
+#get THETA
+theta = lockin.query('OUTP? theta')
+print("THETA:",theta)
+
+
 
 #INSERT CODE HERE
 
